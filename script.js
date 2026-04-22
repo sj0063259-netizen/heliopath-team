fc758374cca22ae749a5060e6f15801a12e29a07
sj0063259-netizen <sj0063259@gmail.com>
2026-04-22 16:59:09 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -28,10 +28,26 @@ async function getWeather() {
 
       // ⚡ Efficiency logic
       let efficiency;
-      if (temp < 35) efficiency = "85%";
-      else if (temp < 30) efficiency = "80%";
-      else if (temp < 25) efficiency = "70%";
-      else efficiency = "60%";
+      if (temp > 40) efficiency = "85%";
+       else if (temp>=39) efficiency = "83%";
+      else if (temp>=38) efficiency = "82%";
+      else if (temp>=37) efficiency = "81%";
+      else if (temp>=36) efficiency = "80%";
+      else if (temp>=35) efficiency = "79%";
+      else if (temp>=34) efficiency = "78%";
+      else if (temp>=33) efficiency = "77%";
+    else if (temp>=32) efficiency = "76%";
+     else if (temp>=31) efficiency = "75%";
+     else if (temp>=30) efficiency = "74%";
+      else if (temp>=29) efficiency = "73%";
+      else if (temp>=28) efficiency = "69%";
+        else if (temp>=27) efficiency = "65%";
+    else if (temp>=26) efficiency = "70%";
+     else if (temp>=25) efficiency = "65%";
+     else if (temp>=24) efficiency = "60%";
+      else if (temp>=23) efficiency = "55%";
+      else if (temp>=21) efficiency = "50%";
+       else efficiency = "40%";
 
       let weather = item.weather[0].main;
 
